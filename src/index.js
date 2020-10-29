f903da77217f1d439d9f3430fd7389392d9a28ca
Erin Davis <51665717+erin-davis@users.noreply.github.com>
2020-10-28 20:27:29 -0500

    edit to projectID in order to deploy

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  projectId: "evernote-clone-89c6e",
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_FIREBASE_APPID,
